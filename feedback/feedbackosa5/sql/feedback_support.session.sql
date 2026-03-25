72d408642c1c43e390805ff6f62243d37a1d8fa7
Rami Nadif <s2401205@edu.bc.fi>
2026-03-25 10:33:56 +0200

    tietokanta

--- feedback/feedbackosa5/sql/feedback_support.session.sql
+++ feedback/feedbackosa5/sql/feedback_support.session.sql
@@ -2,4 +2,26 @@
 
 
 
-/*salasanat: testi, testi2, testi3 */
+/*salasanat: testi, testi2, testi3 
+ALTER TABLE system_user
+ADD passwords varchar(255);
+
+
+
+
+  UPDATE system_user 
+set passwords
+ ="$2b$10$evVzPCEOuk9MLjgCm7jq6Os/DN6gLR1FFbsA/rgsIeJJKBf.lgGb6"
+ where id = "16" 
+
+
+
+UPDATE system_user 
+set passwords
+ ="$2b$10$z9.VRM3HnE2F3uMneus.fO4TMRxu0uEa7fEedXr1/ffwuYfeHNAdC"
+ where id = "15" 
+
+UPDATE system_user 
+set passwords
+ ="$2b$10$evVzPCEOuk9MLjgCm7jq6Os/DN6gLR1FFbsA/rgsIeJJKBf.lgGb6"
+ where id = "14" */
